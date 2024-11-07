2529da949917449dd2152cfafd5587741f736cd1
1249993110 <1249993110@qq.com>
2024-11-07 14:00:58 +0800

    修正拼写错误并更新属性默认值和数据类型
    修正了常量名称中的拼写错误，将 `InvaildValue` 更改为 `InvalidValue`。
    更新了 `Rtd`、`Avg`、`Max`、`Min` 和 `Cou` 属性的默认值引用，使用更正后的 `InvalidValue`。
    将 `Flag` 属性的数据类型从 `InstrumentationDataFlag` 更改为 `string`。

--- IceCoffee.HJ212/Models/PollutantInfo.cs
+++ IceCoffee.HJ212/Models/PollutantInfo.cs
@@ -8,7 +8,7 @@
         /// <summary>
         /// 约定的无效值
         /// </summary>
-        public const decimal InvaildValue = -9999;
+        public const decimal InvalidValue = -9999;
 
         /// <summary>
         /// 污染物因子编码
@@ -19,45 +19,45 @@
         /// 污染物实时采样数据
         /// </summary>
         /// <remarks>
-        /// 默认值为约定的无效值 <see cref="InvaildValue"/>
+        /// 默认值为约定的无效值 <see cref="InvalidValue"/>
         /// </remarks>
-        public decimal Rtd { get; set; } = InvaildValue;
+        public decimal Rtd { get; set; } = InvalidValue;
 
         /// <summary>
         /// 污染物指定时间内平均值
         /// </summary>
         /// <remarks>
-        /// 默认值为约定的无效值 <see cref="InvaildValue"/>
+        /// 默认值为约定的无效值 <see cref="InvalidValue"/>
         /// </remarks>
-        public decimal Avg { get; set; } = InvaildValue;
+        public decimal Avg { get; set; } = InvalidValue;
 
         /// <summary>
         /// 污染物指定时间内最大值
         /// </summary>
         /// <remarks>
-        /// 默认值为约定的无效值 <see cref="InvaildValue"/>
+        /// 默认值为约定的无效值 <see cref="InvalidValue"/>
         /// </remarks>
-        public decimal Max { get; set; } = InvaildValue;
+        public decimal Max { get; set; } = InvalidValue;
 
         /// <summary>
         /// 污染物指定时间内最小值
         /// </summary>
         /// <remarks>
-        /// 默认值为约定的无效值 <see cref="InvaildValue"/>
+        /// 默认值为约定的无效值 <see cref="InvalidValue"/>
         /// </remarks>
-        public decimal Min { get; set; } = InvaildValue;
+        public decimal Min { get; set; } = InvalidValue;
 
         /// <summary>
         /// 污染物指定时间内累计值
         /// </summary>
         /// <remarks>
-        /// 默认值为约定的无效值 <see cref="InvaildValue"/>
+        /// 默认值为约定的无效值 <see cref="InvalidValue"/>
         /// </remarks>
-        public decimal Cou { get; set; } = InvaildValue;
+        public decimal Cou { get; set; } = InvalidValue;
 
         /// <summary>
         /// 检测仪器数据标记
         /// </summary>
-        public InstrumentationDataFlag Flag { get; set; }
+        public string Flag { get; set; }
     }
 }
